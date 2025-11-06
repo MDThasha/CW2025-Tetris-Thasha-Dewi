e24ce50f700972c84c5f00ecc323a9208b33ba71
MDThasha <hfyts2@nottingham.edu.my>
2025-11-06 12:32:57 +0800

    2.7 made bg darker and changed color of borders

--- src/main/java/com/comp2042/Main.java
+++ src/main/java/com/comp2042/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application {
         GuiController c = fxmlLoader.getController();
 
         primaryStage.setTitle("TetrisJFX");
-        Scene scene = new Scene(root, 380, 510);
+        Scene scene = new Scene(root, 370, 510);
         primaryStage.setScene(scene);
         primaryStage.show();
 
